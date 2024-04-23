f7bbf7e16c2b07cab8038e305f215f8770767b6c
AlbertoEinsten <www.albertjuninho@gmail.com>
2024-04-23 18:54:29 -0300

    Inicio do projeto

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+const router = express.Router()
+
+
+router.get("/", (req, res)=>{
+    res.sendFile(path.join(__dirname + "/pages/index.html"))
+})
+
+router.get("/contato", (req, res)=>{
+    res.sendFile(path.join(__dirname + "/pages/contato.html"))
+})
+
+
+app.use(router)
+
+app.listen(4000, ()=>{
+    console.log("SERVIDOR RODANDO")
+})
