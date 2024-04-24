b5cde22852f8c2bf5fbd6983ed1ad9f259b032d4
AlbertoEinsten <www.albertjuninho@gmail.com>
2024-04-23 23:15:06 -0300

    Atualização do projeto

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ const path = require('path')
 const app = express()
 const router = express.Router()
 
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
 
 router.get("/", (req, res)=>{
     res.sendFile(path.join(__dirname + "/pages/index.html"))
